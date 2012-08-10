0f6de1989cd0dcff23c70fff415d971ce6895c44
Herman Banken <hermanbanken@gmail.com>
2012-08-10 17:20:17 +0200

    Tried to get data into GPU.. not really successful
    Signed-off-by: Herman Banken <hermanbanken@gmail.com>

--- tquery/webgl.face.js
+++ tquery/webgl.face.js
@@ -8,7 +8,7 @@ lat = 0, lon = 0, phy = 0, theta = 0;
 var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
 
-init();
+init(cascade, 5, 1);
 
 /** 
  * Insert classifier data into the shader program
@@ -16,85 +16,88 @@ init();
  * include "// INSERT_CLASSIFIERS" in the program 
  * to get it replaced by the data
  */
-function insertClassifiers(shader, cascade) {
+function insertClassifiers(shader, cascade, uniforms) {
+	var push = Array.prototype.push.apply;
 	
-	var str = "ofjewio";
-	return shader.replace("// INSERT_CLASSIFIERS", str);
-
+	var vars = [], main = [], stages = [];
+	
+	function u(k){ return uniforms[k].value; }
+		
+	for(var i = 1; i < 6; i++)
+		vars.push("float haarFeature"+i+"(  ){ return 0.0; }");
+		
+	main.push(
+		//"uniform sampler2D pyr[ "+( u('max_scale') + u('next') * 2 ) * 4 +" ];",
+		//"uniform sampler2D ret[ "+( u('max_scale') + u('next') * 2 ) * 4 +" ];"
+	);
+	
+	main.push("if(1.0 == 1.0){");
+	for(var s = 1; s <= cascade.count; s++){
+		var stage = cascade.stage_classifier[s-1];
+		
+		main.push("if (!stage"+s+"()){ \n\tdiscard; \n}");
+		
+		stages.push(
+			"bool stage"+s+"(){",
+			"	float r = 0.0;", // Placeholder for the sum of the features
+			"	float t = "+stage.threshold+";" // Threshold
+		);
+		for(var f = 0; f < stage.count; f++){
+			var feat = stage.feature[f];
+			stages.push(
+			//	"	r += haarFeature"+feat.size+"();"
+			);
+		}
+		stages.push(
+			"	return r>t;",
+			"}"
+		);
+	}
+	main.push("}");
+	
+	shader = shader.replace("// INSERT_CLASSIFIERS", vars.join("\n"));
+	shader = shader.replace("// INSERT_STAGES", 	 stages.join("\n"));
+	shader = shader.replace("// INSERT_STAGE_CALLS", main.join("\n"));
+	console.log(shader);
+	return shader;
 }
 
 /**
  * Initialize world
  */
-function init() {
+function init(cascade, interval, min_neighbors) {
 	var world = tQuery.createWorld().boilerplate().start();
+	var w = 320, h = 240;
 	
-	world.camera().position.z = 2;
+	world.camera().position.z = 1.7;
 
 	start_time = Date.now();
 
 	uniforms = {
 		time: { type: "f", value: 1.0 },
-		resolution: { type: "v2", value: new THREE.Vector2() },
+		resolution: { type: "v2", value: new THREE.Vector2(w, h) },
+		size: { type: "i", value: w*h },
+		
+		// Following ccv.js the following shared's:
 		texture: { type: "t", value: 1, texture: tQuery.createWebcamTexture() },
-		
-		face_width: { type: "i", value: 1 },
-		face_height: { type: "i", value: 1 },
-		
-		face_num_stages: { type: "i", value: 0 },
-		face_stage_thresholds: { type: "fv1", value: [] },
-		face_stage_num_features: { type: "iv1", value: [] },
-		face_stage_alphas: { type: "fv1", value: [] },
-		
-		face_stage_feature_sizes: { type: "iv1", value: [] },
-		face_stage_feature_nx: { type: "iv1", value: [] },
-		face_stage_feature_ny: { type: "iv1", value: [] },
-		face_stage_feature_nz: { type: "iv1", value: [] },
-		face_stage_feature_px: { type: "iv1", value: [] },
-		face_stage_feature_py: { type: "iv1", value: [] },
-		face_stage_feature_pz: { type: "iv1", value: [] },
+		interval: { type: "i", value: interval },
+		min_neighbors: { type: "i", value: min_neighbors },
+		scale: { type: "i", value: 1 },
+		next: { type: "i", value: 1 },
+		max_scale: { type: "i", value: 1 }
 	};
 	
-	uniforms.face_width.value = cascade.width;
-	uniforms.face_height.value = cascade.height;
-	uniforms.face_num_stages.value = cascade.count;
-	for(var n = 0; n < cascade.count; n++)
-	{
-		// For each stage classifier
-		var stage = cascade.stage_classifier[n];
-		uniforms.face_stage_thresholds.value.push(stage.threshold);
-		uniforms.face_stage_num_features.value.push(stage.count);
-		for(var f = 0; f < stage.count; f++)
-		{
-			// For each feature
-			var feature = stage.feature[f];
-			uniforms.face_stage_feature_sizes.value.push(feature.size);
-			Array.prototype.push.apply(uniforms.face_stage_feature_nx.value, feature.nx);
-			Array.prototype.push.apply(uniforms.face_stage_feature_ny.value, feature.ny);
-			Array.prototype.push.apply(uniforms.face_stage_feature_nz.value, feature.nz);
-			Array.prototype.push.apply(uniforms.face_stage_feature_px.value, feature.px);
-			Array.prototype.push.apply(uniforms.face_stage_feature_py.value, feature.py);
-			Array.prototype.push.apply(uniforms.face_stage_feature_pz.value, feature.pz);
-			
-			// Add two alpha's
-			uniforms.face_stage_alphas.value.push(stage.alpha[f*2], stage.alpha[f*2+1]);
-		}
-	}
-	
-	// N stage_classifiers
-	// - N: threshold
-	// - N: feature_count
-	// - N: array met feature_size's
-	// - N: feature data met nx, ny, ... => 6*feature_size*feature_count
-	// - N: alphas size van feature_count * 2
+	uniforms.scale.value = Math.pow(2, 1 / (interval + 1));
+	uniforms.next.value = interval + 1;
+	uniforms.max_scale.value = Math.floor(Math.log(Math.min(w / cascade.width, h / cascade.height)) / Math.log(uniforms.scale.value));
 
-	uniforms.texture.texture.wrapS = uniforms.texture.texture.wrapT = THREE.Repeat;
+//	uniforms.texture.texture.wrapS = uniforms.texture.texture.wrapT = THREE.Repeat;
 
 	var shaders = [
 		document.getElementById( 'vertexShader' ).textContent,
 		document.getElementById( 'fragmtShader' ).textContent
 	];
-	shaders[1] = insertClassifiers(shaders[1], cascade);
+	shaders[1] = insertClassifiers(shaders[1], cascade, uniforms);
 
 	// Shader material
 	material = new THREE.ShaderMaterial( {
@@ -104,7 +107,7 @@ function init() {
 	} );
 	
 	// Plane displaying material
-	mesh = tQuery.createPlane( 1, 1, 320, 240, material ).addTo(world);
+	mesh = tQuery.createPlane( w/h, 1, 1, 1, material ).addTo(world);
 	
 	// Loop
 	world.loop().hook(render);
